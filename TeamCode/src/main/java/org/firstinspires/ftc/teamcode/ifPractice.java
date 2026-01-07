5e3ffdcac90c8db1c7e6a3a49952b69c21b35d0d
ArnavCooli <155097185+ArnavCooli@users.noreply.github.com>
2026-01-07 08:38:54 -0500

    new files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ifPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ifPractice.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class GamePadPractice extends OpMode {
+public class ifPractice extends OpMode {
 
     @Override
     public void init() {
@@ -13,9 +13,6 @@ public class GamePadPractice extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addData("x", gamepad1.right_stick_x);
-        telemetry.addData("y", gamepad1.right_stick_y);
-        telemetry.addData("a", gamepad1.a);
 
     }
 }
